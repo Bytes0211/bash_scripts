241b875dee88a9b3122173ebe977f3280c93617d
Steven Cotton <cottonbytes@gmail.com>
2022-12-06 13:37:13 +0000

    update

--- advance_course/IO_FileDescriptors.sh
+++ advance_course/IO_FileDescriptors.sh
@@ -2,19 +2,19 @@
 # Including stdin, stdout, stderr values
 
 # This is where file descriptors come into play. A file descriptor is a value of
-# a file that describes what the for is for and how to use it. We have already
-# used $? to get the status status code of a command. Not realizing that the the
+# a file that describes what the file is for and how to use it. We have already
+# used $? to get the status status code of a command. Not realizing that that the
 # value of that status is stored in a file named $?. WFD 1 value tells
 # a command to use the StdOut. and here are the values for IO:
 
 #   File    Valuue
 #   FD 0    StdIn
 #   FD 1    StdOut
-#   FD 1    StdErr
+#   FD 2    StdErr
 
 # Demonstrate the FD value of StdIn (0)
-#Note Debian assumes StdIn when "<" is used 
-echo -e "UNDERSTANDING IO_FILEDESCRIPTION\n"
+# Note Debian assumes StdIn when "<" is used 
+echo -e "UNDERSTANDING IO_FILEDESCRIPTOR\n"
 read X < /etc/os-release
 echo "This is X: ${X}"
 # Explictly refer to StdIn with 0
@@ -23,17 +23,18 @@ read FD 0< /etc/os-release
 echo "This is the File Desriptor: ${FD}"
 # X and FD have the same value
 
-# Deomonstrate the FD value fo StdOut (1)
+# Deomonstrate the FD value for StdOut (1)
 echo "${UID}" > uid-implicit
 echo "Implicit value of UID displayed..."
 sleep 2
 cat uid-implicit
 echo "${UID}" 1> uid-explicit
-echo "Explict valud of UID displayed..."
+echo "Explict value of UID displayed..."
 sleep 2
 cat uid-explicit
 # Demonstrate the FD value of StdErr
 # show the first line of passwd file, the host file and a file that doesnt exist
+# which will introduce and error 
 echo "show the first line of passwd file, the host file and a file that doesnt exist:"
 sleep 2
 head -n1 /etc/passwd /etc/hosts /etc/noneexist > StdOut
@@ -41,7 +42,7 @@ sleep 2
 echo -e "\nredirect the output to file and show content of the file:\n"
 sleep 2
 cat StdOut
-# Note that the error fo the nonexist file did not write to the file
+# Note that the error of the nonexist file did not write to the file
 # To do that you need to explicit call StdErr 
 sleep 2 
 echo -e "\nNote that the error of the nonexist file did not write to the file"
@@ -68,9 +69,9 @@ sleep 3
 # You can append StdOut ">>", Or StdIn "<<" or StdErr "2>>"
 
 # Writing both StdOut and StdErr to the same file
-echo -e "\nYou can send StdOut and StdErr to the same file with: $> <filename>\n"
+echo -e "\nYou can send StdOut and StdErr to the same file with: &> <filename>\n"
 sleep 2
-head -n1 /etc/passwd /etc/hosts /etc/noneexist $> bothStdOutStdErr
+head -n1 /etc/passwd /etc/hosts /etc/noneexist &> bothStdOutStdErr
 cat bothStdOutStdErr
 
 
